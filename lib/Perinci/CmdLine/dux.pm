883068eeb28f9b2c35bc517599968d50c1c01df7
Steven Haryanto <stevenharyanto@gmail.com>
2013-04-16 17:29:21 +0000

    Import of SHARYANTO/App-dux-1.32 from CPAN.
    gitpan-cpan-distribution: App-dux
    gitpan-cpan-version:      1.32
    gitpan-cpan-path:         SHARYANTO/App-dux-1.32.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Perinci/CmdLine/dux.pm
+++ lib/Perinci/CmdLine/dux.pm
@@ -1,30 +1,69 @@
 package Perinci::CmdLine::dux;
+use 5.010;
 use Moo;
 extends 'Perinci::CmdLine';
 
-our $VERSION = '1.31'; # VERSION
+our $VERSION = '1.32'; # VERSION
 
 sub run_subcommand {
-    require Tie::Diamond;
-
     my $self = shift;
 
-    # set `in` and `out` arguments for the dux function
+    # set `in` argument for the dux function
     my $chomp = $self->{_meta}{"x.dux.strip_newlines"} // 1;
+    require Tie::Diamond;
     tie my(@diamond), 'Tie::Diamond', {chomp=>$chomp} or die;
     $self->{_args}{in}  = \@diamond;
-    $self->{_args}{out} = [];
+
+    # set `out` argument for the dux function
+    my $streamo = $self->{_meta}{"x.dux.is_stream_output"};
+    my $fmt = $self->format;
+    if (!defined($streamo)) {
+        # turn on streaming if format is simple text
+        my $iactive;
+        if (-t STDOUT) {
+            $iactive = 1;
+        } elsif ($ENV{INTERACTIVE}) {
+            $iactive = 1;
+        } elsif (defined($ENV{INTERACTIVE}) && !$ENV{INTERACTIVE}) {
+            $iactive = 0;
+        }
+        $streamo = 1 if $fmt eq 'text-simple' || $fmt eq 'text' && !$iactive;
+    }
+    #say "fmt=$fmt, streamo=$streamo";
+    if ($streamo) {
+        die "Can't format stream as $fmt, please use --format text-simple\n"
+            unless $self->format =~ /^text/;
+        $self->{_is_stream_output} = 1;
+        require Tie::Simple;
+        my @out;
+        tie @out, "Tie::Simple", undef,
+            PUSH => sub {
+                my $data = shift;
+                for (@_) {
+                    print $self->format_row($_);
+                }
+            };
+        $self->{_args}{out} = \@out;
+    } else {
+        $self->{_args}{out} = [];
+    }
 
     $self->SUPER::run_subcommand(@_);
 }
 
-sub format_and_display_result {
+sub format_result {
     my $self = shift;
+
+    if ($self->{_is_stream_output}) {
+        $self->{_fres} = "";
+        return;
+    }
+
     if ($self->{_res} && $self->{_res}[0] == 200) {
         # insert out to result, so it can be displayed
         $self->{_res}[2] = $self->{_args}{out};
     }
-    $self->SUPER::format_and_display_result(@_);
+    $self->SUPER::format_result(@_);
 }
 
 1;
@@ -41,7 +80,7 @@ Perinci::CmdLine::dux - Perinci::CmdLine subclass for dux cli
 
 =head1 VERSION
 
-version 1.31
+version 1.32
 
 =head1 DESCRIPTION
 
